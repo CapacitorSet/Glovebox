f6663e2eb90d7bc7bcb8d27c3018043f8783f844
CapacitorSet <9286933+CapacitorSet@users.noreply.github.com>
2019-02-12 13:02:22 +0100

    Add serialization tests

---
+++ tests/serialization.cpp
@@ -0,0 +1,20 @@
+#include <serialization.h>
+#include "gtest/gtest.h"
+#include <rapidcheck/gtest.h>
+#include "FHEContext.cpp"
+
+using Serialization = FHEContext;
+
+TEST_F(Serialization, Serialize) {
+	::rc::detail::checkGTest([=](uint32_t input) {
+		std::stringstream ss;
+		bitspan_t datum = make_bitspan(32, clientParams),
+			out_datum = make_bitspan(32, clientParams);
+		for (int i = 0; i < 32; i++)
+			encrypt(datum[i], (input >> i) & 1, clientParams);
+		serialize(ss, datum, clientParams);
+		deserialize(ss, out_datum, clientParams);
+		for (int i = 0; i < 32; i++)
+			RC_ASSERT(decrypt(datum[i], clientParams) == decrypt(out_datum[i], clientParams));
+	});
+}
