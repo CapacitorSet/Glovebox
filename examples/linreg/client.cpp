40a983a50676f75bc7783487ea46bc4c31ff1b91
CapacitorSet <9286933+CapacitorSet@users.noreply.github.com>
2019-02-28 21:06:04 +0100

    Refactor linreg to use rpclib

--- examples/linreg/client.cpp
+++ examples/linreg/client.cpp
@@ -2,35 +2,14 @@
 #include <cstdio>
 #include <cstring>
 #include <fhe-tools.h>
-#include "../networking.h"
-
-#include "../dyad.h"
+#include <rpc/client.h>
 
 TFHEClientParams_t default_client_params;
 
 using Q4_4 = Fixed<4, 4>;
 
-Array<Q4_4, 2> *xs;
-
-dyad_Stream *s;
-
-void onConnect(dyad_Event*) {
-	send(s, xs);
-}
-
-void onPacket(dyad_Stream*, char *packet, size_t pktsize, char dataType) {
-	puts("Received:");
-	assert(dataType == (Array<Q4_4, 2>::typeID));
-	auto ys = new Array<Q4_4, 2>(packet, pktsize, default_client_params);
-	for (int i = 0; i < 2; i++) {
-		auto y = Q4_4(0, default_client_params);
-		ys->get(y, i);
-		printf("%lf\n", y.toDouble());
-	}
-	delete ys;
-}
-
 int main() {
+	puts("Initializing TFHE...");
 	FILE *secret_key = fopen("secret.key", "rb");
 	if (secret_key == nullptr) {
 		puts("secret.key not found: run ./keygen first.");
@@ -39,26 +18,22 @@ int main() {
 	default_client_params = makeTFHEClientParams(secret_key);
 	fclose(secret_key);
 
-	xs = new Array<Q4_4, 2>(false, default_client_params);
-	auto tmp0 = Q4_4(1.0, default_client_params);
-	xs->put(tmp0, 0);
-	auto tmp1 = Q4_4(1.5, default_client_params);
-	xs->put(tmp1, 1);
+	double plaintext_xs[] = {1.0, 1.5};
+	puts("Constructing values...");
+	Array<Q4_4, 2> xs(false, default_client_params);
+	for (int i = 0; i < 2; i++)
+		xs.put(Q4_4(plaintext_xs[i], default_client_params), i);
 
-	dyad_init();
-
-	s = dyad_newStream();
-	dyad_addListener(s, DYAD_EVENT_CONNECT, onConnect, nullptr);
-	dyad_addListener(s, DYAD_EVENT_DATA, onData, nullptr);
-	dyad_connect(s, "127.0.0.1", 8000);
-
-	while (dyad_getStreamCount() > 0) {
-		dyad_update();
+	puts("Connecting to server...");
+	rpc::client client("127.0.0.1", 8000);
+	puts("Awaiting result...");
+	std::string result = client.call("polyeval", xs.exportToString()).as<std::string>();
+	Array<Q4_4, 2> ys(result.c_str(), result.size(), default_client_params);
+	for (int i = 0; i < 2; i++) {
+		Q4_4 y(0, default_client_params);
+		ys.get(y, i);
+		printf("p(%lf) = %lf\n", plaintext_xs[i], y.toDouble());
 	}
 
-	puts("No more connections, closing.");
-
-	dyad_shutdown();
-	freeTFHEClientParams(default_client_params);
 	return 0;
 }
--- examples/linreg/server.cpp
+++ examples/linreg/server.cpp
@@ -3,54 +3,41 @@
 #include <cstring>
 #include <cstdlib>
 #include <fhe-tools.h>
-#include "../networking.h"
-
-#include "../dyad.h"
+#include <rpc/server.h>
 
 TFHEServerParams_t default_server_params;
 
 using Q4_4 = Fixed<4, 4>;
-Array<Q4_4, 2> *xs;
-
-void onPacket(dyad_Stream *stream, char *packet, size_t pktsize, char dataType) {
-	puts("New packet.");
-	assert(dataType == (Array<Q4_4, 2>::typeID));
-	xs = new Array<Q4_4, 2>(packet, pktsize);
-	auto ys = new Array<Q4_4, 2>();
-	auto p = Polynomial<Q4_4>({1.0, 2.0});
-	bit_t p_overflow = make_bit();
-	for (int i = 0; i < 2; i++) {
-		auto x = Q4_4(0);
-		xs->get(x, i);
-		auto y = p.evaluate(p_overflow, x);
-		ys->put(y, i);
-	}
-	send(stream, ys);
-	// send(stream, p_overflow);
-}
 
 int main() {
+	puts("Initializing TFHE...");
 	FILE *cloud_key = fopen("cloud.key", "rb");
 	if (cloud_key == nullptr) {
 		puts("cloud.key not found: run ./keygen first.");
 		return 1;
 	}
-	puts("Initializing TFHE...");
 	default_server_params = makeTFHEServerParams(cloud_key);
 	fclose(cloud_key);
 
-	dyad_init();
+	rpc::server srv(8000);
 
-	auto stream = dyad_newStream();
-	dyad_addListener(stream, DYAD_EVENT_ACCEPT, onAccept, nullptr);
-	dyad_listen(stream, 8000);
-	printf("Listening: %s:%d\n", dyad_getAddress(stream), dyad_getPort(stream));
+	auto p = Polynomial<Q4_4>({1.0, 2.0});
 
-	while (dyad_getStreamCount() > 0) {
-		dyad_update();
-	}
+	srv.bind("polyeval", [&](std::string _xs) {
+		puts("Received request.");
+		Array<Q4_4, 2> xs(_xs.c_str(), _xs.size());
+		auto ys = Array<Q4_4, 2>();
+		bit_t p_overflow = make_bit();
+		for (int i = 0; i < 2; i++) {
+			auto x = Q4_4(0);
+			xs.get(x, i);
+			auto y = p.evaluate(p_overflow, x);
+			ys.put(y, i);
+		}
+		return ys.exportToString();
+	});
 
-	puts("Exiting gracefully.");
-	dyad_shutdown();
+	srv.run();
+
 	return 0;
 }
