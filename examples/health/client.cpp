158635f9115b4dea20e99a5dbcab13f56fc5f2a2
CapacitorSet <9286933+CapacitorSet@users.noreply.github.com>
2019-02-28 21:16:38 +0100

    Refactor health to use rpclib

--- examples/health/client.cpp
+++ examples/health/client.cpp
@@ -4,27 +4,17 @@
 #include <fstream>
 #include <string>
 #include <fhe-tools.h>
+#include <rpc/client.h>
 
 #include "patient.h"
-#include "../networking.h"
-#include "../dyad.h"
 
 TFHEClientParams_t default_client_params;
-
-dyad_Stream *s;
 
 constexpr int NUM_PATIENTS = 10;
 Array<Patient, NUM_PATIENTS>* records;
 
-void onConnect(dyad_Event*) {
-	puts("Sending...");
-	send(s, records);
-}
-
-void onPacket(dyad_Stream*, char *packet, size_t pktsize, char dataType) {
-}
-
 int main() {
+	puts("Initializing TFHE...");
 	FILE *secret_key = fopen("secret.key", "rb");
 	if (secret_key == nullptr) {
 		puts("secret.key not found: run ./keygen first.");
@@ -40,7 +30,7 @@ int main() {
 	std::getline(file, line);
 	assert(line == "height,weight,age,male");
 
-	records = new Array<Patient, NUM_PATIENTS>(true, default_client_params);
+	auto records = Array<Patient, NUM_PATIENTS>(true, default_client_params);
 
 	for (int i = 0; i < NUM_PATIENTS; i++) {
 		std::getline(file, line);
@@ -51,24 +41,13 @@ int main() {
 		sscanf(line.c_str(), "%lf,%lf,%d,%c\n", &height, &weight, &age, &isMale);
 		printf("%lf,%lf,%d,%d\n", height, weight, age, isMale == '1');
 		Patient p(height, weight, age, isMale == '1', default_client_params);
-		records->put(p, i);
+		records.put(p, i);
 	}
 
-	dyad_init();
+	puts("Connecting to server...");
+	rpc::client client("127.0.0.1", 8000);
+	puts("Uploading database...");
+	client.call("uploadDatabase", records.exportToString());
 
-	s = dyad_newStream();
-	dyad_addListener(s, DYAD_EVENT_CONNECT, onConnect, nullptr);
-	dyad_addListener(s, DYAD_EVENT_DATA, onData, nullptr);
-	puts("Connecting...");
-	dyad_connect(s, "127.0.0.1", 8000);
-
-	while (dyad_getStreamCount() > 0) {
-		dyad_update();
-	}
-	puts("No more connections, closing.");
-
-	dyad_shutdown();
-	freeTFHEClientParams(default_client_params);
-	delete records;
 	return 0;
 }
--- examples/health/server.cpp
+++ examples/health/server.cpp
@@ -2,52 +2,33 @@
 #include <cstdio>
 #include <cstring>
 #include <fhe-tools.h>
+#include <rpc/server.h>
 
 #include "patient.h"
-#include "../networking.h"
-#include "../dyad.h"
 
 TFHEServerParams_t default_server_params;
 
-Array<Patient, 10>* database;
-
-void onPacket(dyad_Stream*, char *packet, size_t pktsize, char dataType) {
-	puts("New packet.");
-	static bool was_database_initialized = false;
-	if (!was_database_initialized) {
-		assert(dataType == ARRAY_TYPE_ID);
-		database = new Array<Patient, 10>(packet, pktsize);
-		was_database_initialized = true;
-		puts("Database initialized.");
-		return;
-	}
-	// If we got here, we have a query.
-	// ...
-}
-
 int main() {
+	puts("Initializing TFHE...");
 	FILE *cloud_key = fopen("cloud.key", "rb");
 	if (cloud_key == nullptr) {
 		puts("cloud.key not found: run ./keygen first.");
 		return 1;
 	}
-	puts("Initializing TFHE...");
 	default_server_params = makeTFHEServerParams(cloud_key);
 	fclose(cloud_key);
 
-	dyad_init();
+	Array<Patient, 10> database;
 
-	auto stream = dyad_newStream();
-	dyad_addListener(stream, DYAD_EVENT_ACCEPT, onAccept, nullptr);
-	dyad_listen(stream, 8000);
-	printf("Listening: %s:%d\n", dyad_getAddress(stream), dyad_getPort(stream));
+	rpc::server srv(8000);
 
-	while (dyad_getStreamCount() > 0) {
-		dyad_update();
-	}
+	srv.bind("uploadDatabase", [&](std::string _db) {
+		puts("Receiving database...");
+		database = Array<Patient, 10>(_db.c_str(), _db.size());
+		puts("Database received.");
+	});
 
-	puts("Exiting gracefully.");
-	dyad_shutdown();
-	delete database;
+	srv.run();
+
 	return 0;
 }
