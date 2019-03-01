a009fe2c9ef202968101cb373648b7a9a979820f
CapacitorSet <9286933+CapacitorSet@users.noreply.github.com>
2019-03-01 20:13:36 +0100

    Drop adder, multiplier primitives

--- include/primitives.h
+++ include/primitives.h
@@ -15,10 +15,6 @@ void _copy(fixed_bitspan_t<N> dst, fixed_bitspan_t<N> src, TFHEServerParams_t p 
 		_copy(dst[i], src[i], p);
 }
 
-// Warning: unlike with the full adder, A and B must not be the same variables as out
-void _half_adder(bit_t out, bit_t carry_out, bit_t A, bit_t B, TFHEServerParams_t p = default_server_params);
-void _full_adder(bit_t out, bit_t carry_out, bit_t A, bit_t B, bit_t carry_in, TFHEServerParams_t p = default_server_params);
-
 void add8(fixed_bitspan_t<8> out, bit_t overflow, fixed_bitspan_t<8>  a, fixed_bitspan_t<8> b, TFHEServerParams_t);
 void add16(fixed_bitspan_t<16> out, bit_t overflow, fixed_bitspan_t<16>  a, fixed_bitspan_t<16> b, TFHEServerParams_t);
 void incr8_if(fixed_bitspan_t<8> out, bit_t cond, fixed_bitspan_t<8> src, TFHEServerParams_t);
