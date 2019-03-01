805a6cffd37cf695bb0eb1904fb0f2699d722304
CapacitorSet <9286933+CapacitorSet@users.noreply.github.com>
2019-03-01 12:59:46 +0100

    Update health example to fix default ctors bug

--- examples/health/patient.h
+++ examples/health/patient.h
@@ -3,6 +3,9 @@
 using Q6_2 = Fixed<6, 2>; // Represents weights from 0 to 64 with a precision of 0.25
 class Patient {
 public:
+	// Beware of implicit initializers! They can cause linker errors about
+	// default_server_params missing in client code, and outright leaks in
+	// server code.
 	Int8 resized_height;
 	Q6_2 weight;
 	Int8 age;
@@ -11,20 +14,27 @@ public:
 	static constexpr int _wordSize = Int8::_wordSize + Q6_2::_wordSize + Int8::_wordSize + 1;
 	static constexpr char typeID = 123;
 
-	Patient(double _height, double _weight, int8_t _age, bool _isMale, TFHEClientParams_t p)
-			: isMale(make_bit(p)) {
-		StructHelper helper(_wordSize, p);
+	Patient(TFHEServerParams_t p) : Patient(StructHelper(_wordSize, p), p) {}
 
+	Patient(double _height, double _weight, int8_t _age, bool _isMale, TFHEClientParams_t p)
+		: Patient(StructHelper(_wordSize, p), p) {
 		int8_t _resized_height = int8_t(_height - 53.0); // Remove offset to make the range fit into an int8
-		this->resized_height = Int8(_resized_height, helper, p);
+		this->resized_height.encrypt(_resized_height, p);
 
 		double _resized_weight = _weight - 32.0; // Remove offset to make the range fit into an int8
-		this->weight = Q6_2(_resized_weight, helper, p);
+		this->weight.encrypt(_resized_weight, p);
 
-		Int8 age(_age, helper, p);
-		this->isMale = helper.make_bit(p);
-		encrypt(isMale, _isMale, p);
+		this->age.encrypt(_age, p);
 
+		encrypt(this->isMale, _isMale, p);
+	}
+
+private:
+	Patient(StructHelper helper, TFHEServerParams_t p)
+		: resized_height(helper, p),
+		weight(helper, p),
+		age(helper, p),
+		isMale(helper.make_bit(p)) {
 		this->data = helper.finalize();
 	}
 };
