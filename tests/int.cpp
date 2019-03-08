95f169b4e0f6c1d0a4afc6b5b79eae2e68220b1b
CapacitorSet <9286933+CapacitorSet@users.noreply.github.com>
2019-03-08 17:39:07 +0100

    Add multiplication tests, merge Sum/SumOverflow

--- tests/int.cpp
+++ tests/int.cpp
@@ -24,23 +24,24 @@ TEST_F(Int8Test, Sum) {
 	::rc::detail::checkGTest([=](int8_t plaintext_a, int8_t plaintext_b) {
 		auto a = Int8(plaintext_a, clientParams);
 		auto b = Int8(plaintext_b, clientParams);
-		auto sum = Int8(serverParams);
-		sum.add(a, b);
-		RC_ASSERT(sum.toInt(clientParams) == int8_t(plaintext_a + plaintext_b));
-	});
-}
-
-TEST_F(Int8Test, SumOverflow) {
-	::rc::detail::checkGTest([=](int8_t plaintext_a, int8_t plaintext_b) {
-		auto a = Int8(plaintext_a, clientParams);
-		auto b = Int8(plaintext_b, clientParams);
 		auto overflow = make_bit(serverParams);
 		auto sum = Int8(serverParams);
 		sum.add(overflow, a, b);
 		int16_t plaintext_sum = plaintext_a + plaintext_b;
 		bool plaintext_overflow = plaintext_sum > std::numeric_limits<int8_t>::max();
 		bool plaintext_underflow = plaintext_sum < std::numeric_limits<int8_t>::min();
+		RC_ASSERT(sum.toInt(clientParams) == int8_t(plaintext_a + plaintext_b));
 		RC_ASSERT((plaintext_overflow || plaintext_underflow) == decrypt(overflow, clientParams));
+	});
+}
+
+TEST_F(Int8Test, Mul) {
+	::rc::detail::checkGTest([=](int8_t plaintext_a, int8_t plaintext_b) {
+		auto a = Int8(plaintext_a, clientParams);
+		auto b = Int8(plaintext_b, clientParams);
+		auto sum = Int8(serverParams);
+		sum.mul(a, b);
+		RC_ASSERT(sum.toInt(clientParams) == int8_t(plaintext_a * plaintext_b));
 	});
 }
 
@@ -68,16 +69,6 @@ TEST_F(Int16Test, Sum) {
 	::rc::detail::checkGTest([=](int16_t plaintext_a, int16_t plaintext_b) {
 		auto a = Int16(plaintext_a, clientParams);
 		auto b = Int16(plaintext_b, clientParams);
-		auto sum = Int16(serverParams);
-		sum.add(a, b);
-		RC_ASSERT(sum.toInt(clientParams) == int16_t(plaintext_a + plaintext_b));
-	});
-}
-
-TEST_F(Int16Test, SumOverflow) {
-	::rc::detail::checkGTest([=](int16_t plaintext_a, int16_t plaintext_b) {
-		auto a = Int16(plaintext_a, clientParams);
-		auto b = Int16(plaintext_b, clientParams);
 		auto overflow = make_bit(serverParams);
 		auto sum = Int16(serverParams);
 		sum.add(overflow, a, b);
@@ -85,5 +76,16 @@ TEST_F(Int16Test, SumOverflow) {
 		bool plaintext_overflow = plaintext_sum > std::numeric_limits<int16_t>::max();
 		bool plaintext_underflow = plaintext_sum < std::numeric_limits<int16_t>::min();
 		RC_ASSERT((plaintext_overflow || plaintext_underflow) == decrypt(overflow, clientParams));
+		RC_ASSERT(sum.toInt(clientParams) == int16_t(plaintext_a + plaintext_b));
+	});
+}
+
+TEST_F(Int16Test, Mul) {
+	::rc::detail::checkGTest([=](int16_t plaintext_a, int16_t plaintext_b) {
+		auto a = Int16(plaintext_a, clientParams);
+		auto b = Int16(plaintext_b, clientParams);
+		auto sum = Int16(serverParams);
+		sum.mul(a, b);
+		RC_ASSERT(sum.toInt(clientParams) == int16_t(plaintext_a * plaintext_b));
 	});
 }
