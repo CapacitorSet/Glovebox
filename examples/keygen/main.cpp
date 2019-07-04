2b5bd6f7a8280c7d066494d38553fbaece030437
CapacitorSet <9286933+CapacitorSet@users.noreply.github.com>
2019-07-04 23:38:37 +0200

    Format keygen

--- examples/keygen/main.cpp
+++ examples/keygen/main.cpp
@@ -20,22 +20,24 @@ int main(int argc, char *argv[]) {
 
 	const int minimum_lambda = 110;
 	printf("Parameters: minimum_lambda=%d\n", minimum_lambda);
-	TFheGateBootstrappingParameterSet* params = new_default_gate_bootstrapping_parameters(minimum_lambda);
+	TFheGateBootstrappingParameterSet *params =
+	    new_default_gate_bootstrapping_parameters(minimum_lambda);
 
-	uint32_t seed[] = { 314, 1592, 657 };
+	uint32_t seed[] = {314, 1592, 657};
 	tfhe_random_generator_setSeed(seed, 3);
 	puts("Seed: ");
 	for (size_t i = 0; i < sizeof(seed) / sizeof(seed[0]); i++)
 		printf("%d ", seed[i]);
 	puts("");
-	TFheGateBootstrappingSecretKeySet* key = new_random_gate_bootstrapping_secret_keyset(params);
+	TFheGateBootstrappingSecretKeySet *key =
+	    new_random_gate_bootstrapping_secret_keyset(params);
 
-	secret_key = fopen("secret.key","wb");
+	secret_key = fopen("secret.key", "wb");
 	export_tfheGateBootstrappingSecretKeySet_toFile(secret_key, key);
 	fclose(secret_key);
 	puts("Secret key saved to secret.key.");
 
-	cloud_key = fopen("cloud.key","wb");
+	cloud_key = fopen("cloud.key", "wb");
 	export_tfheGateBootstrappingCloudKeySet_toFile(cloud_key, &key->cloud);
 	fclose(cloud_key);
 	puts("Cloud key saved to cloud.key.");
